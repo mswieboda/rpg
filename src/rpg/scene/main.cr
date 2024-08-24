4b30b35191eb796ccd8a116c7938a4c92ff9dfff
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-08-23 19:51:53 -0500

    testing newlines

--- src/rpg/scene/main.cr
+++ src/rpg/scene/main.cr
@@ -17,7 +17,8 @@ module RPG::Scene
       @level = Levels::World.new(player)
       HUD.init
 
-      @text = GSF::StyledText.new("testing, [s=ibu oc=#00ff0066 fc=#0000ff]test[/s] and then [s= fc=#ff0000]something[/s] else!", Font.default, 16)
+      @text = GSF::StyledText.new("testing, \nfoo and bar [s=ibu oc=#00ff0066 fc=#0000ff]test[/s] and then [s= fc=#ff0000]another \nthing[/s] else!", Font.default, 16)
+      @text.line_spacing = 12
       @text.outline_thickness = 1
       @text.position = {300, 300}
     end
