29c4537710ec31df4959d125a3e484a54310515b
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-08-25 19:52:54 -0500

    moves bag ui draw item to item class

--- src/rpg/bag_ui.cr
+++ src/rpg/bag_ui.cr
@@ -9,12 +9,6 @@ module RPG
     TitleFontSize = 16
     TitleTextColor = SF::Color::White
 
-    ItemLabelFontSize = 12
-    ItemLabelTextColor = SF::Color::White
-    ItemLabelBottomPadding = 8
-    ItemAmountFontSize = 10
-    ItemAmountTextColor = SF::Color::White
-
     Padding = 16
     CellCols = 8
 
@@ -28,8 +22,6 @@ module RPG
     @@y : Float32?
     @@background_rect : SF::RectangleShape?
     @@title_text : SF::Text?
-    @@item_label_text : SF::Text?
-    @@item_amount_text : SF::Text?
     @@grid_cell_size : Float32?
 
     def self.update(keys, joysticks)
@@ -100,32 +92,6 @@ module RPG
       text
     end
 
-    def self.item_label_text : SF::Text
-      if text = @@item_label_text
-        return text
-      end
-
-      text = SF::Text.new("", Font.default, ItemLabelFontSize)
-      text.fill_color = ItemLabelTextColor
-
-      @@item_label_text = text
-
-      text
-    end
-
-    def self.item_amount_text : SF::Text
-      if text = @@item_amount_text
-        return text
-      end
-
-      text = SF::Text.new("2", Font.default, ItemAmountFontSize)
-      text.fill_color = ItemAmountTextColor
-
-      @@item_amount_text = text
-
-      text
-    end
-
     def self.draw(window : SF::RenderWindow, bag : Bag)
       return unless show?
 
@@ -140,77 +106,9 @@ module RPG
 
       bag.items.values.in_slices_of(CellCols).each_with_index do |items, row|
         items.each_with_index do |item, col|
-          draw_item(window, item, dx, dy, row, col)
+          item.draw_ui(window, grid_cell_size, x + dx, y + dy, row, col)
         end
       end
-    end
-
-    def self.draw_item(window, item, dx, dy, row, col)
-      ix = x + dx + col * grid_cell_size
-      iy = y + dy + row * grid_cell_size
-
-      # icon
-      radius = grid_cell_size / 4
-      circle = SF::CircleShape.new(radius)
-      circle.fill_color = item.fill_color
-      circle.position = {
-        ix + radius,
-        iy + radius
-      }
-
-      window.draw(circle)
-
-      # label
-      lines = word_wrap_lines(item.name, grid_cell_size)
-
-      lines.each_with_index do |line, line_index|
-        item_label_text.string = line
-        line_width = item_label_text.global_bounds.width
-
-        item_label_text.position = {
-          ix + grid_cell_size / 2 - line_width / 2,
-          iy + grid_cell_size + ItemLabelFontSize * line_index - ItemLabelFontSize - ItemLabelFontSize / 2 * lines.size - ItemLabelBottomPadding
-        }
-
-        window.draw(item_label_text)
-      end
-
-      if item.amount > 1
-        item_amount_text.string = item.amount.to_s
-        item_amount_text.position = {ix + radius * 2.5, iy + radius * 2.5}
-
-        window.draw(item_amount_text)
-      end
-    end
-
-    def self.word_wrap_lines(text, width)
-      item_label_text.string = text
-      label_width = item_label_text.global_bounds.width
-
-      return [text] if label_width < width
-
-      lines = [""]
-      line_index = 0
-      words = text.split
-      item_label_text.string = ""
-
-      words.each_with_index do |word, index|
-        item_label_text.string += word
-        item_label_text.string += " " unless index >= words.size - 1
-        line_width = item_label_text.global_bounds.width
-
-        if line_width >= width
-          line_index += 1
-          lines << ""
-          item_label_text.string = word
-          item_label_text.string += " " unless index >= words.size - 1
-        end
-
-        lines[line_index] += word
-        lines[line_index] += " " unless index >= words.size - 1
-      end
-
-      lines
     end
   end
 end
--- src/rpg/item.cr
+++ src/rpg/item.cr
@@ -1,9 +1,18 @@
 module RPG
   class Item
+    LabelFontSize = 12
+    LabelTextColor = SF::Color::White
+    LabelBottomPadding = 8
+    AmountFontSize = 10
+    AmountTextColor = SF::Color::White
+
     getter name : String
     getter key : String
     getter amount : Int32
     getter fill_color : SF::Color
+
+    @@label_text : SF::Text?
+    @@amount_text : SF::Text?
 
     def initialize(@key : String, @name : String, @fill_color = SF::Color.new)
       @amount = 1
@@ -61,6 +70,32 @@ module RPG
       )
     end
 
+    def self.label_text : SF::Text
+      if text = @@label_text
+        return text
+      end
+
+      text = SF::Text.new("", Font.default, LabelFontSize)
+      text.fill_color = LabelTextColor
+
+      @@label_text = text
+
+      text
+    end
+
+    def self.amount_text : SF::Text
+      if text = @@amount_text
+        return text
+      end
+
+      text = SF::Text.new("2", Font.default, AmountFontSize)
+      text.fill_color = AmountTextColor
+
+      @@amount_text = text
+
+      text
+    end
+
     def add
       @amount += 1
     end
@@ -71,6 +106,78 @@ module RPG
 
     def empty?
       amount <= 0
+    end
+
+    def self.word_wrap_lines(text, width)
+      label_text.string = text
+      label_width = label_text.global_bounds.width
+
+      return [text] if label_width < width
+
+      lines = [""]
+      line_index = 0
+      words = text.split
+      label_text.string = ""
+
+      words.each_with_index do |word, index|
+        label_text.string += word
+        label_text.string += " " unless index >= words.size - 1
+        line_width = label_text.global_bounds.width
+
+        if line_width >= width
+          line_index += 1
+          lines << ""
+          label_text.string = word
+          label_text.string += " " unless index >= words.size - 1
+        end
+
+        lines[line_index] += word
+        lines[line_index] += " " unless index >= words.size - 1
+      end
+
+      lines
+    end
+
+    def draw_ui_icon(window, radius, x, y)
+      circle = SF::CircleShape.new(radius)
+      circle.fill_color = fill_color
+      circle.position = {x + radius, y + radius}
+
+      window.draw(circle)
+    end
+
+    def draw_ui_label(window, grid_cell_size, radius, x, y)
+      lines = self.class.word_wrap_lines(name, grid_cell_size)
+      label_text = self.class.label_text
+      amount_text = self.class.amount_text
+
+      lines.each_with_index do |line, line_index|
+        label_text.string = line
+        line_width = label_text.global_bounds.width
+
+        label_text.position = {
+          x + grid_cell_size / 2 - line_width / 2,
+          y + grid_cell_size + LabelFontSize * line_index - LabelFontSize - LabelFontSize / 2 * lines.size - LabelBottomPadding
+        }
+
+        window.draw(label_text)
+      end
+
+      if amount > 1
+        amount_text.string = amount.to_s
+        amount_text.position = {x + radius * 2.5, y + radius * 2.5}
+
+        window.draw(amount_text)
+      end
+    end
+
+    def draw_ui(window, grid_cell_size, dx, dy, row, col)
+      x = dx + col * grid_cell_size
+      y = dy + row * grid_cell_size
+      radius = grid_cell_size / 4
+
+      draw_ui_icon(window, radius, x, y)
+      draw_ui_label(window, grid_cell_size, radius, x, y)
     end
   end
 end
